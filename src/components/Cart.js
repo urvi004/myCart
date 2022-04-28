939a6fb901e685d3cc83f78577f65be0c20b996c
urvi004 <urvisharma54@gmail.com>
2022-04-28 08:25:28 +0530

    remove and add product to cart

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,6 +1,13 @@
-import React from 'react'
+import React from 'react';
+import { CartState} from '../context/Context'
 
 const Cart = () => {
+  const {
+    state: {cart},
+    dispatch
+  } = CartState();
+
+
   return (
     <div>Cart</div>
   )
